69a49671772ea1002719c7c6dc3c9b683accc08d
Adrian Toth <t.adrian88@gmail.com>
2020-10-06 16:03:31 +0100

    Added features: top_up, deduct, error_msg(limit)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,20 @@
 # can hold credit to allow users to pay for public transport use
 
 class Oystercard
-  
+
+  attr_reader :balance
+
+  LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
-  def top_up
+  def top_up(money)
+    raise "Error, top up limit is #{LIMIT}" if @balance + money > 90
+    @balance = @balance + money
+  end
+
+  def deduct(fare)
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,22 @@ describe Oystercard do
   it 'starts with the balance of zero' do
     expect(subject.balance).to eq 0
   end
+
+  it 'customer can top up' do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it 'tops up the balance' do
+    subject.top_up(5)
+    expect(subject.balance).to eq 5
+  end
+
+  it 'raises error if limit exceeded' do
+    expect{subject.top_up(91)}.to raise_error "Error, top up limit is #{Oystercard::LIMIT}"
+  end
+
+  it 'it deducts the fare from balance' do
+    expect(subject).to respond_to(:deduct).with(1).argument
+  end
+
 end
