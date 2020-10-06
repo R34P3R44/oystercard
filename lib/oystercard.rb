f99127d89f1939d377cc27fc502bccc820bf41fa
Adrian Toth <61809619+R34P3R44@users.noreply.github.com>
2020-10-06 14:21:07 +0100

    Update oystercard.rb

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,26 +1,11 @@
 # can hold credit to allow users to pay for public transport use
 
 class Oystercard
- 
-  attr_reader :balance
-  MAX_VALUE = 90
   
   def initialize
     @balance = 0
   end
 
-<<<<<<< HEAD
   def top_up
-=======
-  def top_up(amount) 
-    raise "Cannot top-up: Balance cannot be over £90" if max_credit?(amount)
-    @balance += amount
-  end
-
-  private
-  
-  def max_credit?(amount)
-    @balance + amount > 90
->>>>>>> 61987e4a5e044f5e20595a0ad5a4973b4a28b630
   end
 end
